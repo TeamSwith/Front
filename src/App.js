68a50c6fa36f946b00ec81ca7e603d32b9632180
Imseongeun <sungyun0719@gmail.com>
2024-10-29 16:53:07 +0900

    footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Link 제거
 import MainPage from './pages/MainPage';
 import CreateStudy from './pages/CreateStudy';
 import ManageStudy from './pages/ManageStudy';
+import Header from './components/Header';
+import Footer from './components/Footer'; 
 
 const App = () => {
   return (
     <Router>
-      <header className="bg-gray-800 text-white p-4">
-        <nav className="flex space-x-4">
-          <Link to="/" className="hover:text-gray-400">메인 페이지</Link>
-          <Link to="/create-study" className="hover:text-gray-400">스터디 생성</Link>
-          <Link to="/manage-study" className="hover:text-gray-400">스터디 관리</Link>
-        </nav>
-      </header>
+      <Header /> {/* 헤더 컴포넌트 사용 */}
       <main className="p-4">
-      <Routes>
+        <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/create-study" element={<CreateStudy />} />
           <Route path="/manage-study" element={<ManageStudy />} />
-      </Routes>
+        </Routes>
       </main>
+      <Footer /> {/* Footer 컴포넌트 추가 */}
     </Router>
   );
 };
