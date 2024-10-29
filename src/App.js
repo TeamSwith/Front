13db77ab976720f621c42d9e06b50d13479a2b91
soyoung01 <5040unng@gmail.com>
2024-10-29 15:26:22 +0900

    header 변경

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
 import MainPage from './pages/MainPage';
 import CreateStudy from './pages/CreateStudy';
 import ManageStudy from './pages/ManageStudy';
+import logo from './swithLogo.png';
 
 const App = () => {
   return (
     <Router>
-      <header className="bg-gray-800 text-white p-4">
-        <nav className="flex space-x-4">
-          <Link to="/" className="hover:text-gray-400">메인 페이지</Link>
-          <Link to="/create-study" className="hover:text-gray-400">스터디 생성</Link>
-          <Link to="/manage-study" className="hover:text-gray-400">스터디 관리</Link>
-        </nav>
+      <header className="bg-[#8CC29E] text-white p-4 flex justify-between items-center">
+        {/* 왼쪽: 로고와 네비게이션 링크 */}
+        <div className="flex items-center space-x-4">
+          {/* 로고 (메인 페이지로 이동) */}
+          <Link to="/" className="flex items-center">
+            <img src={logo} alt="Swith Logo" className="h-10 mr-2" />
+          </Link>
+
+          {/* 네비게이션 링크 */}
+          <nav className="flex space-x-4">
+            <Link to="/create-study" className="text-white hover:text-gray-100">스터디 생성</Link>
+            <Link to="/manage-study" className="text-white hover:text-gray-100">스터디 관리</Link>
+          </nav>
+        </div>
+
+        {/* 오른쪽: My Page 및 알람 버튼 */}
+        <div className="flex space-x-4">
+          <button className="text-white hover:text-gray-100">My Page</button>
+          <button className="text-white hover:text-gray-100">알람</button>
+        </div>
       </header>
+
       <main className="p-4">
         <Routes>
           <Route path="/" element={<MainPage />} />
