be49265ca4602b2cf14aecd1ac23541e7977f622
soyoung01 <5040unng@gmail.com>
2024-11-10 15:10:39 +0900

    editManage 구현

--- src/components/EditSIdebar.js
+++ src/components/EditSIdebar.js
@@ -1,55 +1,60 @@
 import React from 'react';
 import xIcon from "../assets/X.png";
+import timeIcon from "../assets/timeEdit.png";
+import locationIcon from "../assets/location.png";
 
 const EditSidebar = ({ scheduleData, tasks, handleCheckboxChange, setIsEditing }) => {
   return (
     <div className="w-full">
-    <div className="flex flex-col w-full flex-grow bg-[#F7F9F2] p-6 rounded-lg shadow-lg mb-4 md:mb-0 mt-4 md:mt-0">
-        <div>
-            <div className="flex justify-between items-center mb-4">
-                <h2 className="text-lg font-semibold text-[#4B4B4B]">
-                    {scheduleData.date} Edit Schedule
-                </h2>
-                <img src={xIcon} alt="Back" className="w-6 h-6 cursor-pointer"
-                onClick={() => setIsEditing(false)} />
-            </div>
-          <hr className="border-t-[2px] border-gray-300 mb-4" />
-            <form>
-                <label className="block mb-2 text-[#4B4B4B]">시간:</label>
-                <input type="text" className="w-full p-2 mb-4 border border-gray-300 rounded-lg" />
-                <label className="block mb-2 text-[#4B4B4B]">장소:</label>
-                <input type="text" className="w-full p-2 mb-4 border border-gray-300 rounded-lg" />
-            </form>
-
-          <hr className="border-t-[2px] border-gray-300 mb-2" />
-          <div className="p-6">
-            <div className="flex items-center mb-4 space-x-4">
-              <p className="text-lg text-[#4B4B4B] mr-4">과제</p>
-            </div>
-
+        <div className="flex flex-col w-full flex-grow bg-[#F7F9F2] p-6 rounded-lg shadow-lg mb-4 md:mb-0 mt-4 md:mt-0">
             <div>
-              {tasks.map((task) => (
-                <div key={task.id} className="flex items-center mb-2.5">
-                  <input
-                    type="checkbox"
-                    checked={task.checked}
-                    onChange={() => handleCheckboxChange(task.id)}
-                    className="mr-2"
-                  />
-                  <label className="text-[#4B4B4B]">{task.label}</label>
+                <div className="flex justify-between items-center mb-4">
+                    <h2 className="text-lg font-semibold text-[#4B4B4B]">
+                        {scheduleData.date} Edit Schedule
+                    </h2>
+                    <img src={xIcon} alt="Back" className="w-6 h-6 cursor-pointer"
+                    onClick={() => setIsEditing(false)} />
                 </div>
-              ))}
+            <hr className="border-t-[2px] border-gray-300 mb-4" />
+                <div className="flex justify-between items-center mb-2">
+                    <p className="text-[#4B4B4B]">
+                        <strong>시간:</strong> {scheduleData.time}</p>
+                    <img src={timeIcon} ali="time edit" className="w-6 h-6 cursor-pointer" />
+                </div>
+                <div className="flex justify-between items-center mb-2">
+                    <p className="text-[#4B4B4B] mb-2"><strong>장소:</strong> {scheduleData.location}</p>
+                    <img src={locationIcon} ali="location edit" className="w-6 h-6 cursor-pointer" />
+                </div>
+
+            <hr className="border-t-[2px] border-gray-300 mb-2" />
+            <div className="p-6">
+                <div className="flex items-center mb-4 space-x-4">
+                <p className="text-lg text-[#4B4B4B] mr-4">과제</p>
+                </div>
+
+                <div>
+                    {tasks.map((task) => (
+                        <div key={task.id} className="flex items-center mb-2.5">
+                        <input
+                            type="checkbox"
+                            checked={task.checked}
+                            onChange={() => handleCheckboxChange(task.id)}
+                            className="mr-2"
+                        />
+                        <label className="text-[#4B4B4B]">{task.label}</label>
+                        </div>
+                    ))}
+                </div>
             </div>
-          </div>
-          <hr className="border-t-[2px] border-gray-300 mb-4" />
-        </div>    
-    </div>
+            <hr className="border-t-[2px] border-gray-300 mb-4" />
+            </div>    
+        </div>
 
     <div className="flex justify-end">
         <button type="submit" className="bg-[#8CC29E] text-white px-4 py-2 rounded-lg mt-3 ml-auto">
             저장
         </button>
-        </div>
+    </div>
     </div>
 
   );
--- src/components/ManageSidebar.js
+++ src/components/ManageSidebar.js
@@ -10,11 +10,10 @@ const ManageSidebar = ({ activeTab, setActiveTab, progressPercentage, tasks, han
 
         <div className="flex space-x-8 w-fit border-b border-gray-300 mb-4">
             <button
-            onClick={() => setActiveTab('schedule')}
-            className={`text-lg font-semibold pb-2 ${
-              activeTab === 'schedule' ? 'text-[#8CC29E] border-b-2 border-[#8CC29E]' : 'text-gray-500'
-            }`}
-            >
+                onClick={() => setActiveTab('schedule')}
+                className={`text-lg font-semibold pb-2 ${
+                activeTab === 'schedule' ? 'text-[#8CC29E] border-b-2 border-[#8CC29E]' : 'text-gray-500'
+                }`} >
               Schedule
             </button>
             <button
@@ -22,10 +21,10 @@ const ManageSidebar = ({ activeTab, setActiveTab, progressPercentage, tasks, han
               className={`text-lg font-semibold pb-2 ${
               activeTab === 'information' ? 'text-[#8CC29E] border-b-2 border-[#8CC29E]' : 'text-gray-500'
             }`}
-          >
+            >
               Information
             </button>
-          </div>
+        </div>
     
 
         <div className="bg-[#F7F9F2] flex-grow p-6 rounded-lg shadow-lg mb-4 md:mb-0 mt-4 md:mt-0">
@@ -33,53 +32,53 @@ const ManageSidebar = ({ activeTab, setActiveTab, progressPercentage, tasks, han
         {activeTab === 'schedule' ? (
             scheduleData ? (
                 <div>
-                <div className="flex justify-between items-center mb-4">
-                    <h2 className="text-lg font-semibold text-[#4B4B4B]">
-                    {scheduleData.date} Study Schedule
-                    </h2>
-                    <div className="flex space-x-3">
-                        <img src={editIcon} alt="Edit Icon" className="w-6 h-6 cursor-pointer"
-                        onClick={onEditClick} />
-                        <img src={trashIcon} alt="Trash Icon" className="w-6 h-6 cursor-pointer" />
-                    </div>
-                </div>
-                <hr className="border-t-[2px] border-gray-300 mb-4" />
-                <p className="text-[#4B4B4B] mb-2"><strong>시간:</strong> {scheduleData.time}</p>
-                <p className="text-[#4B4B4B] mb-2"><strong>장소:</strong> {scheduleData.location}</p>
-                <p className="text-[#4B4B4B]">{scheduleData.description}</p>
-                
-                <hr className="border-t-[2px] border-gray-300 mb-2" />
-                <div className="p-6">
-                    {/* 과제 텍스트와 게이지 바 컨테이너 */}
-                    <div className="flex items-center mb-4 space-x-4">
-                    <p className="text-lg text-[#4B4B4B] mr-4">과제</p>
-                    <div className="w-[200px] h-4 bg-gray-300 rounded-full overflow-hidden">
-                        <div
-                        className="h-full bg-[#8CC29E] rounded-full transition-width duration-300"
-                        style={{ width: `${progressPercentage}%` }}
-                        ></div>
-                    </div>
-                    <span className="text-[#5B5B5B]">
-                        {Math.round(progressPercentage)}%
-                    </span>
-                    </div>
-                    {/* 체크박스 리스트 */}
-                    <div>
-                    {tasks.map((task) => (
-                        <div key={task.id} className="flex items-center mb-2.5">
-                        <input
-                            type="checkbox"
-                            checked={task.checked}
-                            onChange={() => handleCheckboxChange(task.id)}
-                            className="mr-2"
-                        />
-                        <label className="text-[#4B4B4B]">{task.label}</label>
+                    <div className="flex justify-between items-center mb-4">
+                        <h2 className="text-lg font-semibold text-[#4B4B4B]">
+                        {scheduleData.date} Study Schedule
+                        </h2>
+                        <div className="flex space-x-3">
+                            <img src={editIcon} alt="Edit Icon" className="w-6 h-6 cursor-pointer"
+                            onClick={onEditClick} />
+                            <img src={trashIcon} alt="Trash Icon" className="w-6 h-6 cursor-pointer" />
                         </div>
-                    ))}
                     </div>
-                </div>
-                <hr className="border-t-[2px] border-gray-300 mb-4" />
-                <ManageComments />
+                    <hr className="border-t-[2px] border-gray-300 mb-4" />
+                    <p className="text-[#4B4B4B] mb-2"><strong>시간:</strong> {scheduleData.time}</p>
+                    <p className="text-[#4B4B4B] mb-2"><strong>장소:</strong> {scheduleData.location}</p>
+                    <p className="text-[#4B4B4B]">{scheduleData.description}</p>
+                    
+                    <hr className="border-t-[2px] border-gray-300 mb-2" />
+                    <div className="p-6">
+                        {/* 과제 텍스트와 게이지 바 컨테이너 */}
+                        <div className="flex items-center mb-4 space-x-4">
+                            <p className="text-lg text-[#4B4B4B] mr-4">과제</p>
+                            <div className="w-[200px] h-4 bg-gray-300 rounded-full overflow-hidden">
+                                <div
+                                className="h-full bg-[#8CC29E] rounded-full transition-width duration-300"
+                                style={{ width: `${progressPercentage}%` }}
+                                ></div>
+                            </div>
+                            <span className="text-[#5B5B5B]">
+                                {Math.round(progressPercentage)}%
+                            </span>
+                        </div>
+                        {/* 체크박스 리스트 */}
+                        <div>
+                            {tasks.map((task) => (
+                                <div key={task.id} className="flex items-center mb-2.5">
+                                <input
+                                    type="checkbox"
+                                    checked={task.checked}
+                                    onChange={() => handleCheckboxChange(task.id)}
+                                    className="mr-2"
+                                />
+                                <label className="text-[#4B4B4B]">{task.label}</label>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <hr className="border-t-[2px] border-gray-300 mb-4" />
+                    <ManageComments />
                 </div>
             ) : (
                 <p className="text-gray-500">해당 날짜의 스케줄이 없습니다.</p>
@@ -102,7 +101,7 @@ const ManageSidebar = ({ activeTab, setActiveTab, progressPercentage, tasks, han
 
         </div>
 
-        </div>
+    </div>
         );
     };
     
