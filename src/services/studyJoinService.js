1004c61c72cfa9bd0b9007fcdf5195c248b31fdf
Imseongeun <sungyun0719@gmail.com>
2024-11-29 03:46:32 +0900

    중간저장

--- src/services/studyJoinService.js
+++ src/services/studyJoinService.js
@@ -10,12 +10,23 @@ export const checkStudyJoin = async (groupInsertId, groupPw) => {
   
     // 가입 여부만 체크해서 success 값 반환
     if (response.data.message === "이미 가입되어 있음") {
-        return { success: false }; // 이렇게 반환해야 맞게 나감
-      } else {
         return { success: true }; // 이렇게 반환해야 맞게 나감
+      } else {
+        return { success: false }; // 이렇게 반환해야 맞게 나감
       }
     } catch (error) {
       console.error('스터디 그룹 가입 오류:', error);
       throw error;
     }
 };
+
+// // 스터디 가입하기 (groupId와 userId를 매핑테이블에 짝지어 저장)
+// export const joinStudy = async (groupId) => {
+//     try {
+//       const response = await api.post(`${API_BASE_URL}/user-group/create`, { groupId });
+//       return response.data;
+//     } catch (error) {
+//       console.error('스터디 그룹 가입 요청 오류:', error);
+//       throw error;
+//     }
+//   };
